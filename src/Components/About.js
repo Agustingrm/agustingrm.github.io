6597ca363d95b97e96fc8400569c4af60daafca9
Agustin Gramajo <agustingramajo@gmail.com>
2021-10-31 15:58:10 -0300

    Improvement in responsive

--- src/Components/About.js
+++ src/Components/About.js
@@ -10,13 +10,11 @@ function About() {
   return (
     <section id="about" className="aboutContainer">
       <h2>About Me</h2>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce vel turpis fringilla nulla semper vehicula. Sed at elit
-        eros. Proin tempor ligula risus, ac tempor nunc eleifend eget. Sed lobortis diam at neque tristique, at rhoncus lacus
-        lobortis. Sed a consectetur dui. Donec eu condimentum sem. Praesent ipsum ante, faucibus sit amet convallis at, pharetra
-        non tellus. In quis est vitae turpis gravida placerat vel vitae nulla.
-      </p>
       <img src={face} alt="Agustin Gramajo Gaitan" loading="lazy" className="face" />
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce vel turpis fringilla nulla semper vehicula. Sed at elit eros.
+      Proin tempor ligula risus, ac tempor nunc eleifend eget. Sed lobortis diam at neque tristique, at rhoncus lacus lobortis.
+      Sed a consectetur dui. Donec eu condimentum sem. Praesent ipsum ante, faucibus sit amet convallis at, pharetra non tellus.
+      In quis est vitae turpis gravida placerat vel vitae nulla.
       <div>
         <h3>Languages I speak:</h3>
         <div className="languageContainer">
