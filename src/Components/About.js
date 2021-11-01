dd7552c82fb30dffa99efc4e0cd4cfaf314a2e5d
Agustin Gramajo <agustingramajo@gmail.com>
2021-11-01 20:09:07 -0300

    Added about info

--- src/Components/About.js
+++ src/Components/About.js
@@ -11,10 +11,10 @@ function About() {
     <section id="about" className="aboutContainer">
       <h2>About Me</h2>
       <img src={face} alt="Agustin Gramajo Gaitan" loading="lazy" className="face" />
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce vel turpis fringilla nulla semper vehicula. Sed at elit eros.
-      Proin tempor ligula risus, ac tempor nunc eleifend eget. Sed lobortis diam at neque tristique, at rhoncus lacus lobortis.
-      Sed a consectetur dui. Donec eu condimentum sem. Praesent ipsum ante, faucibus sit amet convallis at, pharetra non tellus.
-      In quis est vitae turpis gravida placerat vel vitae nulla.
+      Hello! My name is Agustin, I'm an Argentinean/Spanish industrial engineer. After graduating I decided to go deeper into the
+      world of programming, so I studied in Argentinean universities and on my own through www.theodinproject.com languages
+      related to web development. <br/>These last months I started to do a master in project management, while I continue with my
+      programming studies. At the moment I'm looking for my first experience in front end development. 
       <div>
         <h3>Languages I speak:</h3>
         <div className="languageContainer">
